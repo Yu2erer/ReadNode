6c42de4c02a14700ba27a6089d39c5d68ee4e45d
Yu2erer <yu2erer@gmail.com>
2017-08-08 22:16:36 +0800

    remove storyBoard

---
+++ ReadNode/ReadNode/Classes/View/RNMainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RNMainViewController.swift
+//  ReadNode
+//
+//  Created by ntian on 2017/8/8.
+//  Copyright © 2017年 ntian. All rights reserved.
+//
+
+import UIKit
+
+class RNMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
