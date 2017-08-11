05a735c10c4ea6712209c3a5e4fb56bd4dc9352a
Yu2erer <yu2erer@gmail.com>
2017-08-12 01:50:36 +0800

    添加 Discover 控制器中 所需要的 addButton 素材

--- ReadNode/ReadNode/Classes/View/Discover/RNDiscoverViewController.swift
+++ ReadNode/ReadNode/Classes/View/Discover/RNDiscoverViewController.swift
@@ -22,5 +22,10 @@ class RNDiscoverViewController: UIViewController {
 extension RNDiscoverViewController {
     fileprivate func setupUI() {
         navigationItem.titleView = UILabel.titleView(text: "Discover", textColor: UIColor.nt_color(hex: 0x34394B), font: UIFont(name: "PingFang", size: 12))
+        // addBtn
+        let addBtn = UIButton(type: .custom)
+        addBtn.frame = CGRect(x: view.bounds.width - 12 - 72, y: view.bounds.height - 12 - 72 - 64 - 49, width: 72, height: 72)
+        addBtn.setImage(UIImage(named: "addButton"), for: .normal)
+        view.addSubview(addBtn)
     }
 }
