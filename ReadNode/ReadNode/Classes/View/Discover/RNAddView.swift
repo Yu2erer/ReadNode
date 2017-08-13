cbdf54922808269472d1ea87724cfc539cb19283
Yu2erer <yu2erer@gmail.com>
2017-08-13 23:09:13 +0800

    addView 样式完成

--- ReadNode/ReadNode/Classes/View/Discover/RNAddView.swift
+++ ReadNode/ReadNode/Classes/View/Discover/RNAddView.swift
@@ -10,11 +10,27 @@ import UIKit
 
 @objc protocol RNAddViewDelegate: NSObjectProtocol {
     @objc optional func didClickClose()
+    @objc optional func didClickSave()
 }
 class RNAddView: UIView {
 
+    @IBOutlet weak var textView: UITextView!
     weak var viewDelegate: RNAddViewDelegate?
+    
+    @IBAction func addHttp(_ sender: UIButton) {
+    }
+    
+    @IBAction func addHttps(_ sender: UIButton) {
+    }
+    
+    @IBAction func addXml(_ sender: UIButton) {
+    }
+    
+    
     @IBAction func close(_ sender: UIButton) {
         viewDelegate?.didClickClose?()
+    }
+    @IBAction func save(_ sender: UIButton) {
+        viewDelegate?.didClickSave?()
     }
 }
