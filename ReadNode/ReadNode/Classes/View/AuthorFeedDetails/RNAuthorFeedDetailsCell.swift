bee37296da9b6d8771f2451156cf963b6aa1cfe4
Yu2erer <yu2erer@gmail.com>
2017-08-20 14:43:10 +0800

    添加 RNAuthorFeedDetailsCell 并布局完成

---
+++ ReadNode/ReadNode/Classes/View/AuthorFeedDetails/RNAuthorFeedDetailsCell.swift
@@ -0,0 +1,18 @@
+//
+//  RNAuthorFeedDetailsCell.swift
+//  ReadNode
+//
+//  Created by ntian on 2017/8/20.
+//  Copyright © 2017年 ntian. All rights reserved.
+//
+
+import UIKit
+
+class RNAuthorFeedDetailsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+}
